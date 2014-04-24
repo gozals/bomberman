bced2a1db7fa684a69f6bb4c526c53325797ee6d
Chafic Najjar <chafic.najjar@gmail.com>
2014-04-25 00:27:20 +0300

    added sprite animation for all directions

--- js/player.js
+++ js/player.js
@@ -1,13 +1,16 @@
 
-function Player(sprite, board, name, x, y) {
+function Player(sprite_sheet, board, name, x, y) {
     this.name = name;
     this.x = x;
     this.y = y;
+
     this.left = false;
     this.right = false;
     this.up = false;
     this.down = false;
-    this.alive = true;
+
+    this.direction = "down";
+
     this.stand = true;
     this.toggle = false;
     this.release_bomb = false;
@@ -15,17 +18,53 @@ function Player(sprite, board, name, x, y) {
     this.sprite_width = 12;
     this.sprite_height = 18;
 
+    this.alive = true;
+
+    this.vertical_walk = 0;
 }
 
 Player.LENGTH = 40;
 
-// All instances of Person create reference methods to it's prototype.
-// These references are not deletable (but they can be overwritten).
 Player.prototype.draw = function() {
-    if (this.stand)
-        context.drawImage(sprite, 8, 2, 15, 22, this.x+7, this.y, 15+this.sprite_width, 22+this.sprite_height);
-    else
-        context.drawImage(sprite, 8+15, 2, 15, 22, this.x+7, this.y, 15+this.sprite_width, 22+this.sprite_height);
+    var sprite = [];
+
+    // To-do: find an alternative to this.stand (eg: use %)
+    // To-do: create a complete sprite sheet using the current one
+
+    switch (this.direction) {
+        case "left":    // Note: there's another sprite to add
+            if (this.stand)
+                sprite = fetch_sprite("horizontal_walk_1");
+            else
+                sprite = fetch_sprite("horizontal_walk_2");
+            break;
+
+        case "up":
+            if(this.stand)
+                sprite = fetch_sprite("vertical_walk_3");
+            else
+                sprite = fetch_sprite("vertical_walk_4");
+            break;
+
+        case "right":   // Note: there's another sprite to add
+            if (this.stand)
+                sprite = fetch_sprite("horizontal_walk_3");
+            else
+                sprite = fetch_sprite("horizontal_walk_4");
+            break;
+
+        case "down":
+            if (this.stand)
+                sprite = fetch_sprite("vertical_walk_1");
+                //context.drawImage(sprite_sheet, 8, 2, 15, 22, this.x+7, this.y, 15+this.sprite_width, 22+this.sprite_height);
+            else
+                sprite = fetch_sprite("vertical_walk_2");
+                //context.drawImage(sprite_sheet, 8+15, 2, 15, 22, this.x+7, this.y, 15+this.sprite_width, 22+this.sprite_height);
+            break;
+    }
+
+    // (sprite_sheet, src_x, src_y, src_width, src_height, dest_x, dest_y, dest_width, dest_height);
+    context.drawImage(sprite_sheet, sprite[0], sprite[1], sprite[2], sprite[3], this.x+7, this.y, sprite[2]*(Player.LENGTH/sprite[3]), Player.LENGTH);//sprite[3]+this.sprite_height);
 }
 
 Player.prototype.move = function() {
@@ -37,26 +76,30 @@ Player.prototype.move = function() {
         if (board[this.y/Player.LENGTH][this.x/Player.LENGTH] >= 1)
             this.x += Player.LENGTH;
         this.left = false;
+        this.direction = "left";
     }
 
-    if (this.up) {
+    else if (this.up) {
         this.y -= Player.LENGTH;
         if (board[this.y/Player.LENGTH][this.x/Player.LENGTH] >= 1)
             this.y += Player.LENGTH;
         this.up = false;
+        this.direction = "up";
     }
 
-    if (this.right) {
+    else if (this.right) {
         this.x += Player.LENGTH;
         if (board[this.y/Player.LENGTH][this.x/Player.LENGTH] >= 1)
             this.x -= Player.LENGTH;
         this.right = false;
+        this.direction = "right";
     }
 
-    if (this.down) {
+    else if (this.down) {
         this.y += Player.LENGTH;
         if (board[this.y/Player.LENGTH][this.x/Player.LENGTH] >= 1)
             this.y -= Player.LENGTH;
         this.down = false;
+        this.direction = "down";
     }
 }
--- js/utils.js
+++ js/utils.js
@@ -13,3 +13,24 @@ function rect(x, y, w, h) {
     context.stroke();
 }
 
+function fetch_sprite (sprite_name) {
+    var test;
+    switch(sprite_name) {
+        case "vertical_walk_1":
+            return [8, 2, 15, 22];
+        case "vertical_walk_2":
+            return [23, 2, 15, 22];
+        case "vertical_walk_3":
+            return [195, 2, 15, 22];
+        case "vertical_walk_4":
+            return [212, 2, 15, 22];
+        case "horizontal_walk_1":
+            return [93, 2, 15, 22];
+        case "horizontal_walk_2":
+            return [108, 2, 15, 22];
+        case "horizontal_walk_3":
+            return [228, 28, 15, 22];
+        case "horizontal_walk_4":
+            return [213, 28, 15, 22];
+    }
+}
